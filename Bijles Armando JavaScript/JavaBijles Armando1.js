173fa99bdaf6308494292ff0a4401659ea44b9e1
JensondeGroot <kaizersmr@gmail.com>
2023-03-15 14:16:23 +0100

    Bijles Armando JavaScript dag 1

---
+++ Bijles Armando JavaScript/JavaBijles Armando1.js
@@ -0,0 +1,70 @@
+//Console
+
+//Variabelen
+ //Declaration
+ let naam = "Armando"
+ console.log(naam);
+
+//nummers
+
+
+ //strings
+
+
+//boolean
+
+let boolean = true;
+console.log(boolean);
+//Operators
+let hello = "hello"
+let world = "world"
+
+console.log(hello+world);
+
+
+let num = 5;
+ num +=5;
+ console.log(num);
+
+let num = 5;
+let num2 = 10;
+
+let num3 = num+num2;
+ console.log(num3); 
++
+-
+*
+/
+
+let boolean = true;
+let boolean = !boolean;
+console.log(boolean1);
+
+//if statements
+
+let boolean == true;
+if(5<10 && boolean){
+    console.log("je bent slim");
+
+}
+Image(boolean){
+    console.log("waar");
+}
+
+//if else en else
+let num = 2;
+if(num>3){
+    console.log("de if statement lukte")
+}else if(true){
+    console log("de else statement lukte")
+}else{
+
+
+}
+
+//Termary operator
+
+let boolean = true;
+let boolean = false;
+let num = (boolean&&boolean1)?2:5;
+console.log(num);
